71dc422233ce345b7f43b10b8df1307baea727d5
Sungmin yi <ssuyi@ucdavis.edu>
2017-07-09 08:49:20 -0700

    Delete index3.js

--- storage/index3.js
+++
@@ -1,9 +0,0 @@
-let fileButton = document.querySelector('#fileButton');
-
-fileButton.addEventListener('change', function(e) {
-    let file = e.target.files[0];
-    let storageRef = firebase.storage().ref('/sound/' + file.name);
-    storageRef.put(file);
-    console.log(file)
-
-})
